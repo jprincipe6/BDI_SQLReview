f843290b9a78615a6ef5494ce6af4f84c7182f41
jprincipe6 <jeanpaul.principe@gmail.com>
2022-10-01 16:21:13 +0200

    First Test of Paper entity: addRow un delete

--- src/main/java/com/sqlreview/App.java
+++ src/main/java/com/sqlreview/App.java
@@ -1,13 +1,37 @@
 package com.sqlreview;
 
+import com.sqlreview.entity.Paper;
+import com.sqlreview.exception.DaoException;
+import com.sqlreview.facade.entity.PaperOperatorFacadeImpl;
+
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static final Logger LOGGER =
+            Logger.getLogger(App.class.getName());
+
+    private static PaperOperatorFacadeImpl paperOperatorFacade;
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        init();
+
+        Paper testPaper = new Paper(1,"test_1","abstract_1");
+
+        try{
+            paperOperatorFacade.addRow(testPaper);
+        }catch (DaoException ex){
+            throw new RuntimeException(ex);
+        }
+
+        paperOperatorFacade.deleteRow(testPaper);
+    }
+
+    private static void init (){
+        paperOperatorFacade = PaperOperatorFacadeImpl.getInstance();
     }
 }
