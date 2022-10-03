6db462700124e133cb2125c38c236319d2633273
jprincipe6 <jeanpaul.principe@gmail.com>
2022-10-03 21:26:23 +0200

    Finish the sentence SQL

--- resources/Queries.sql
+++ resources/Queries.sql
@@ -1,12 +1,55 @@
-select count(*) as numberConference, ranking from conference group by ranking order by ranking desc;
 
-select paper.title, paper.abstract, author.name from paper inner join writes on writes.paperid = paper.paperid inner
-	join author on author.authorid = writes.authorid where author.name in ('Moises','Karl');
 
-  select author.name, cites.paperIdFrom, cites.paperIdto from author inner join writes on  writes.authorId = author.authorId inner join
-	cites on cites.paperIdfrom = writes.paperId where cites.paperIdFrom = cites.paperIdto;
+SELECT count(*) AS numberOfConferences, ranking
+    FROM conference
+    GROUP BY ranking
+    ORDER BY ranking ASC;
 
-  CREATE VIEW PublishesIn AS select writes.authorId, conference.confId from writes inner join submits on submits.paperId = writes.paperid
-  inner join conference on conference.confId = submits.confId
-  	where submits.isAccepted = TRUE group by(writes.authorId,conference.confId)
-  	order by writes.authorid desc;
+SELECT paper.title, paper.abstract, author.name
+    FROM paper INNER JOIN
+            writes ON writes.paperid = paper.paperid INNER JOIN
+                author ON author.authorid = writes.authorid
+    WHERE author.name LIKE ('Ga%')
+        OR author.name LIKE ('Gr%');
+
+
+SELECT author.name, cites.paperIdFrom, cites.paperIdto
+    FROM author
+        INNER JOIN writes
+            ON  writes.authorId = author.authorId
+        INNER JOIN cites
+            ON cites.paperIdto = writes.paperId
+    WHERE
+        author.authorId IN (SELECT writes.authorId
+            FROM writes
+            WHERE cites.paperIdFrom = writes.paperId);
+
+CREATE VIEW PublishesIn AS
+  SELECT writes.authorId, conference.confId
+  FROM writes
+      INNER JOIN submits
+        ON submits.paperId = writes.paperid
+      INNER JOIN conference
+        ON conference.confId = submits.confId
+  WHERE submits.isAccepted = TRUE
+  GROUP BY (writes.authorId,conference.confId)
+  ORDER BY writes.authorid desc;
+
+
+
+SELECT paper.title
+    FROM paper
+        INNER JOIN writes
+            ON writes.paperId = paper.paperId
+        INNER JOIN author
+            ON author.authorId = writes.authorId
+    WHERE
+        author.name Like ('A%')
+        AND
+        writes.paperId IN (
+                SELECT writes.paperId
+                   FROM paper
+                        INNER JOIN writes
+                           ON writes.paperId = paper.paperId
+                       GROUP BY (writes.paperID)
+                       HAVING COUNT(writes.paperId)> 1);
